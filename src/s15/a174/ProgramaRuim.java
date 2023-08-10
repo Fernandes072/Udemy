dc3c49fc1d9bf4c6bcdd2dfcc5024362a8630911
Fernandes072 <joaofilho8274@gmail.com>
2023-08-10 00:18:40 -0300

    Exceção - Sulução Ruim

---
+++ src/s15/a174/ProgramaRuim.java
@@ -0,0 +1,43 @@
+package s15.a174;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class ProgramaRuim {
+
+	public static void main(String[] args) throws ParseException {
+
+		Scanner ler = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		System.out.print("Número do quarto: ");
+		int numero = ler.nextInt();
+		System.out.print("Data checkin (dd/mm/yyyy): ");
+		Date checkin = sdf.parse(ler.next());
+		System.out.print("Data checkout (dd/mm/yyyy): ");
+		Date checkout = sdf.parse(ler.next());
+
+		if (checkin.before(checkout)) {
+			Reserva res1 = new Reserva(numero, checkin, checkout);
+			System.out.println(res1);
+			System.out.println();
+
+			System.out.print("Data checkin (dd/mm/yyyy): ");
+			checkin = sdf.parse(ler.next());
+			System.out.print("Data checkout (dd/mm/yyyy): ");
+			checkout = sdf.parse(ler.next());
+
+			String erro = res1.atualizarDatas(checkin, checkout);
+			if (erro != null) {
+				System.out.println(erro);
+			}
+			System.out.println(res1);
+		} else {
+			System.out.println("Erro: Data de checkout deve ser depois da checkin");
+		}
+
+		ler.close();
+	}
+}
--- src/s15/a174/Reserva.java
+++ src/s15/a174/Reserva.java
@@ -5,27 +5,35 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reserva {
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
+
 	private Integer numeroQuarto;
 	private Date checkin;
 	private Date checkout;
-	
+
 	public Reserva(Integer numeroQuarto, Date checkin, Date checkout) {
 		this.numeroQuarto = numeroQuarto;
 		this.checkin = checkin;
 		this.checkout = checkout;
 	}
-	
+
 	public long duracao() {
 		long diferenca = checkout.getTime() - checkin.getTime();
-		return TimeUnit.DAYS.convert(diferenca,TimeUnit.MILLISECONDS);
+		return TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS);
 	}
-	
-	public void atualizarDatas(Date checkin, Date checkout) {
+
+	public String atualizarDatas(Date checkin, Date checkout) {
+		Date agora = new Date();
+		if (checkin.after(agora) || checkout.after(agora)) {
+			return "Erro: Data  de atualização deve ser futura";
+		}
+		if (checkin.before(checkout)) {
+			return "Erro: Data de checkout deve ser depois da checkin";
+		}
 		this.checkin = checkin;
 		this.checkout = checkout;
+		return null;
 	}
 
 	public Integer getNumeroQuarto() {
@@ -43,8 +51,8 @@ public class Reserva {
 	public Date getCheckout() {
 		return checkout;
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		StringBuilder sb = new StringBuilder("Reserva: ");
 		sb.append("Quarto: " + numeroQuarto);
 		sb.append(", checkin: " + sdf.format(checkin));
